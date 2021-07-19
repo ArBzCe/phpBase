ca5d3a2bf1ac88a3a881cf5791a18491c6fe95ee
thh <thh@qq.com>
2021-07-19 10:51:31 +0800

    新建数据类型

---
+++ phpDataType.php
@@ -0,0 +1,30 @@
+<?php
+    # 数据类型 10 种
+
+    ## 4 种标量
+    ### bool 布尔型
+        $exampleBoolean = true;
+        $exampleBoolean = false;
+    ### int 整型
+        $exampleInteger = 1;
+        $exampleInteger = 0;
+        $exampleInteger = -1;
+    ### float 浮点型
+        $exampleFloat = 1.0;
+    ### string 字符型
+        $exampleString = "1";
+        $exampleString = '1';
+
+    ## 4 种复合类型
+    ### array 数组
+        $exampleArray = array(1,2);
+    ### object 对象
+        $exampleObject = obj (new class{ function fun(){}});
+    ### callable 可回调
+
+    ### iterable 可迭代
+
+    ## 2 种特殊类型
+    ### resource 资源
+
+    ### NULL 无类型
